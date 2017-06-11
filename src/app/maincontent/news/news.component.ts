1f3e463490ba03a1b86486a69fe1f24b3f9e2ba3
Olivier <olivier_boniteau@yahoo.fr>
2017-06-11 14:31:05 +0200

    CSS

--- src/app/maincontent/news/news.component.ts
+++ src/app/maincontent/news/news.component.ts
@@ -21,8 +21,6 @@ export class NewsComponent implements OnInit {
 
   type = 'news';
 
-  progressvalue = 20;
-
 
     constructor(
         private conf: ConfService,
@@ -32,20 +30,20 @@ export class NewsComponent implements OnInit {
     ) {
     }
     ngOnInit(): void {
-        this.progressvalue = 40;
+
         this.dataService.getAll(this.type)
                                   .subscribe((data: any[]) => this.items = data,
                                       error => this.log.debug(this.type + ' ' + error),
                                       () =>  {
-                                        this.progressvalue = 60;
+
                                         if (this.max > 0 && this.items.length > this.max) {
                                            this.items = this.items.slice(this.items.length - this.max, this.items.length);
 
                                         }
                                         this.log.debug(this.type + ' '  +  this.items.length);
-                                        this.progressvalue = 80;
+
                                         this.orderby.transform(this.items, 'date', 'desc');
-                                        this.progressvalue = 100;
+
                                     });
 
 
