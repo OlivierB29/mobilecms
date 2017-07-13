9e4580c50e977c6fe8d224b65435da00bf15b14e
Olivier <olivier_boniteau@yahoo.fr>
2017-07-13 08:54:07 +0200

    organize imports

--- src/app/sidenav/menubutton.component.ts
+++ src/app/sidenav/menubutton.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { MenuItem } from '../../shared/model/menuitem';
+import { MenuItem } from './menuitem';
 import { Router } from '@angular/router';
 
 @Component({
