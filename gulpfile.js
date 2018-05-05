99385076c5fc81ff11bd264073d1c8603c0a04c6
OlivierB29 <olivier_boniteau@yahoo.fr>
2018-05-05 19:06:53 +0200

    gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -2,36 +2,41 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 const del = require('del');
 
-/**
-*
-*/
-var projectName = 'mobilecms';
-var projectVersion = '0.1.5';
+let projectName = 'mobilecms';
+let projectVersion = '0.1.6';
 
-gulp.task('cleandist', function() {
-  del(['dist/media', 'dist/public', 'dist/assets']).then(paths => {
-      console.log('Deleted files and folders:\n', paths.join('\n'));
-  });
+gulp.task('default', defaultTask);
 
+function defaultTask(done) {
 
-});
-
-gulp.task('release', function() {
+    console.log('gulp cleandist #clean unused assets from dist');
+    console.log('gulp release #create release file');
+  done();
+}
 
 
+gulp.task('release', releaseTask);
+
+function releaseTask(done) {
   var archiveFile = 'release/'+projectName + '-' + projectVersion + '.zip';
   console.log('Creating : ' + archiveFile);
 
   gulp.src('dist/*')
         .pipe(zip(archiveFile))
-        .pipe(gulp.dest('dist'))
+        .pipe(gulp.dest('dist'));
+  done();
+}
 
 
-});
+gulp.task('cleandist', cleandistTask);
 
-gulp.task('default', function() {
-
-  console.log('gulp cleandist #clean unused assets from dist');
-  console.log('gulp release #create release file');
-
-});
+function cleandistTask(done) {
+    let assets = [
+   'dist/'+projectName+'/media',
+   'dist/'+projectName+'/public',
+   'dist/'+projectName+'/assets'];
+  del(assets).then(paths => {
+      console.log('Deleted files and folders:\n', paths.join('\n'));
+  });
+  done();
+}
